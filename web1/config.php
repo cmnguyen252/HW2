70a4d4510081c70b34e54c6e1144758c6e34e6f8
cmnguyen252 <cmnguyen252@gmail.com>
2025-02-26 17:10:11 -0600

    HW2

---
+++ web1/config.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = 'db';
+$databaseUsername = 'test';
+$databasePassword = 'test';
+$databaseName = 'example';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
